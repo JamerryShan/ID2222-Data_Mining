7be040b9e7542ad747ac58a80300a290d1ac3752
yuxinmmmm <1123534279@qq.com>
2021-11-16 17:28:06 +0100

    add files

--- README.md
+++ README.md
@@ -1,23 +1,23 @@
 # ID2222-Data-Mining
 
-### Homework 1: Finding Similar Items: Textually Similar Documents
+## Homework 1: Finding Similar Items: Textually Similar Documents
 
 This is the submission of homework 1 of the data mining(ID2222) course, finding similar items: textually similar documents, which is based on Python.
 
-#### library dependencies
+ #### library dependencies
 
-```hw1.py```  
+ + ```hw1.py```  
 itertools  
 numpy  
 math  
 time  
-```prepocess.py```   
+ + ```prepocess.py```   
 unicodedata  
 re  
-```dataset_reader.py```  
++ ```dataset_reader.py```  
 os  
 json  
 #### Run
 run with ```python3 hw1.py```
 
-### Homework 2: Discovery of Frequent Itemsets and Association Rules
+## Homework 2: Discovery of Frequent Itemsets and Association Rules
--- hw2/hw2.py
+++ hw2/hw2.py
@@ -15,5 +15,32 @@ print(len(data_set))
 # Finding frequent itemsets with support at least s
 # implement the A-Priori algorithm
 
-# def A_Prior(buckets, threshold):
+def A_Prior(baskets, threshold):
+    frequent_item = []
+    # find the frequency of each item in all buckets
+    item_frequency = {}
+    for basket in baskets:
+        for item in basket:
+            # if this item didn't exist in previous baskets
+            if item not in item_frequency:
+                item_frequency[item] = 1
+            else:
+                item_frequency[item] += 1
+    # get items whose support >= threshold
+    for item, support in item_frequency.items():
+        if support >= threshold:
+            frequent_item.append(item)
 
+    return frequent_item
+
+item_frequency = A_Prior(data_set, 20)
+print(len(item_frequency))
+print(item_frequency)
+
+# get larger_frequent_sets from frequent_items, result contains k+1 items while input_set contains k items
+def larger_frequent_sets(frequent_sets, frequent_items):
+
+    for itemset in frequent_sets:
+        for item in frequent_items:
+            # add item that not be included in itemsets to set
+            itemset
