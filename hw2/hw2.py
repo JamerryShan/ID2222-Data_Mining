c79a6efb52984c31dc7f94f21228051545ff61c3
yuxinmmmm <yuxinm@kth.se>
2021-11-19 23:10:04 +0100

    A-Priori Algorithm

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -14,6 +14,54 @@ print(len(data_set))
 
 # Finding frequent itemsets with support at least s
 # implement the A-Priori algorithm
+# get larger_frequent_sets from frequent_items, result contains k+1 items while input_set contains k items
+def larger_frequent_sets(frequent_sets, frequent_items):
+    new_freq_itemsets = []
+    possible_sets = set()
+
+    for itemset in frequent_sets:
+        for item in frequent_items:
+            # add item that not be included in itemsets to set
+            if type(itemset) == list:
+                possibleSet = list(itemset)
+            else:
+                possibleSet = [itemset]
+            if item not in possibleSet:
+                possibleSet.append(item)
+                possibleSet.sort()
+                # make sure the new itemsets are unique
+                # add new_set to possible_sets to store and compare sets
+                new_set = ",".join(map(str, possibleSet))
+                if new_set not in possible_sets:
+                    possible_sets.add(new_set)
+                    # if not add in new sets before
+                    new_freq_itemsets.append(possibleSet)
+
+    return new_freq_itemsets
+
+#filter itemsets appears' time more than threshold
+def filter(baskets, possibleSets, threshold):
+    #initial the counter to count
+    occurences = [0 for s in range(len(possibleSets))]
+
+    for one_basket in baskets:
+        for index, sets in enumerate(possibleSets):
+            sets_not_in_basket = False
+            #to ckeck if the item in possiblesets is also in the basket
+            for item in sets:
+                if item not in one_basket:
+                    sets_not_in_basket = True
+            # add counter
+            if sets_not_in_basket == False:
+                occurences[index] += 1
+    # filter the occurences >= threshold
+    frequent_sets = {}
+    for i, v in enumerate(occurences):
+        if v >= threshold:
+            _str = ",".join(map(str, possibleSets[i]))
+            frequent_sets[_str] = v
+    return frequent_sets
+
 
 def A_Prior(baskets, threshold):
     frequent_item = []
@@ -31,18 +79,19 @@ def A_Prior(baskets, threshold):
         if support >= threshold:
             frequent_item.append(item)
 
-    return frequent_item
+    # use frequent single item to creat frequent k-item_sets
+    frequent_item_sets = frequent_item
+    while len(frequent_item_sets) > 0:
+        print(frequent_item_sets)
+        possible_freq_itemsets = larger_frequent_sets(frequent_item_sets, frequent_item)
+        frequent_item_sets = []
+        for sets, v in filter(baskets, possible_freq_itemsets, threshold).items():
+            frequent_item_sets.append(list(map(int, sets.split(","))))
 
-item_frequency = A_Prior(data_set, 20)
-print(len(item_frequency))
-print(item_frequency)
+    return frequent_item_sets
 
-# get larger_frequent_sets from frequent_items, result contains k+1 items while input_set contains k items
-def larger_frequent_sets(frequent_sets, frequent_items):
+item_frequency = A_Prior(data_set[:1500], 15)
+# print(len(item_frequency))
+# print(item_frequency)
 
-    for itemset in frequent_sets:
-        for item in frequent_items:
-            # add item that not be included in itemsets to set
-            if len(itemset) == 1:
-                itemset = [itemset]
-            itemset.append(item)
+
